6ef202e087aa2b305eeb5e818f215d054f248f46
Atif Afsar <atifafsar70@gmail.com>
2026-02-23 16:23:09 +0530

    Initialize the BrandsWay Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Pages/Home'
+import Navbar from './Components/common/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
